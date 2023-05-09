19fddf6d4687536c50fb3fc76980def877ad1db8
Gombert Gwenael <gwenael.gombert@etudiant.univ-rennes1.fr>
2023-05-09 21:38:16 +0000

    Update HanoiApp.java

--- hanoi/src/main/java/fr/istic/hanoi/HanoiApp.java
+++ hanoi/src/main/java/fr/istic/hanoi/HanoiApp.java
@@ -4,14 +4,14 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class HanoiApp extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HanoiApp.class.getResource("hanoi-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hanoi");
         stage.setScene(scene);
         stage.show();
